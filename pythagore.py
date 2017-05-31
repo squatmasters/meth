4899030cbfb84cc97c6b8a550669c76b79578785
speherh <spencer@speherh.com>
2017-05-31 14:00:46 -0400

    pythagore.check()

--- pythagore.py
+++ pythagore.py
@@ -2,5 +2,20 @@
 # calculations with the Pythagorean theorem.
 
 def check() {
-  pass
+  a = input('a = ')
+  b = input('b = ')
+  c = input('c = ')
+  a = float(a)
+  b = float(b)
+  c = float(c)
+  a = a**2
+  b = b**2
+  c = c**2
+  a = eval(a)
+  b = eval(b)
+  c = eval(c)
+  if a + b == c:
+    print("This is a right triangle.")
+  else:
+    print("This is not a right triangle.")
 }
